ad2b7605307c6ddb1df32098f8cd3622ea02fe74
Imroz0512 <86233499+Imroz0512@users.noreply.github.com>
2023-09-22 20:35:55 +0000

    difference b/w dictionary or list
    lect 52 importent

---
+++ lect52_devlopment_fundamental.py
@@ -0,0 +1,33 @@
+# diffrent b\w dictionary and list
+dictionary = {
+  'a':[2,4,6],
+  'c':'imroz',
+  'd':True,
+}
+print(dictionary['a'][2])
+"""
+1 it is a key vlue pair it store lot of a value and diffirent type of data structure
+
+ 2 and dictionary is a unorderd  data structure 
+ 
+ 3 It hold more information then list
+ """
+my_list = [
+   {
+  'a':[2,4,6],
+  'c':'imroz',
+  'd':True,
+},
+ {
+  'a':[2,4,6],
+  'c':'imroz',
+  'd':True,
+}
+]
+print(my_list[0]['a'][2])
+"""
+1 It have a single valalue pair
+data structure
+2 It is a orderd form of data  structure
+3 It holds less value it have a simply index which hold its value
+"""
